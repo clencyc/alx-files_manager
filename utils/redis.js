1f7d00d2951e9c794f560f23bccdf1951f756028
clencyc <clency2023@gmail.com>
2024-10-03 00:51:32 +0000

    redis task 1

---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.getAsync = promisify(this.client.get).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
